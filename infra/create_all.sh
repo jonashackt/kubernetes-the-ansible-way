d973c6e7ef943db319a24e0f33da34fa1d6fe15d
Jonas Hecht <jonas.hecht@codecentric.de>
2018-08-08 19:18:35 +0200

    Refactoring of names: now 3 configuration steps are named accordingly (all configure_x). Also master-configuration role is now named as `k8s-master`, same as the workers already with `k8s-worker`

--- infra/create_all.sh
+++ infra/create_all.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 vagrant up
 ansible-playbook install.yml -i hosts
-ansible-playbook start_controllers.yml -i hosts
-ansible-playbook start_workers.yml -i hosts
+ansible-playbook configure_masters.yml -i hosts
+ansible-playbook configure_workers.yml -i hosts
 ansible-playbook configure_clients.yml -i hosts
