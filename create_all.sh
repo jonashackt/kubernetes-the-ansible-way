020285a145d00578d18438ef5438445e9cf3aa00
Jonas Hecht <jonas.hecht@codecentric.de>
2018-08-09 14:01:44 +0200

    Renamed install.yml to create-certs-and-config.yml. Therefore moved loabbalancer installation on init to configure-external-access.yml

--- create_all.sh
+++ create_all.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 vagrant up
-ansible-playbook install.yml -i hosts
-ansible-playbook configure_masters.yml -i hosts
-ansible-playbook configure_workers.yml -i hosts
-ansible-playbook configure_clients.yml -i hosts
+ansible-playbook create-certs-and-config.yml -i hosts
+ansible-playbook configure-masters.yml -i hosts
+ansible-playbook configure-workers.yml -i hosts
+ansible-playbook configure-external-access.yml -i hosts
